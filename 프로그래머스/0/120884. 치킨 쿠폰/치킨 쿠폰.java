b7eb02088b686d279c1ec02f829515506190fb4b
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2024-07-18 16:09:46 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+
+        int coupon = chicken;
+        while (coupon >= 10) {
+            chicken = coupon / 10;
+            coupon %= 10;
+            coupon += chicken;
+            answer += chicken;
+        }
+
+        return answer;
+    }
+}
