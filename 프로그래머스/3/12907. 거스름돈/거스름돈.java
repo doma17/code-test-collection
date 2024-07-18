2816db550711f03bdf1acdfdd9cbf0712bcacba7
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2024-07-18 21:17:47 +0900

    [level 3] Title: 거스름돈, Time: 28.47 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[100001];
+        dp[0] = 1;
+
+        for (int m : money) {
+            for (int i = m; i <= n; i++) {
+                dp[i] += dp[i - m];
+            }
+        }
+
+        return dp[n];
+    }
+}
