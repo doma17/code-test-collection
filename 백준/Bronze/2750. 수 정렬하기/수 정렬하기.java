ac06950c4cefe9de69a7396b937312e06602609c
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-05-21 14:47:19 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 16172 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        // Init
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (var x : arr) {
+            sb.append(x + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
