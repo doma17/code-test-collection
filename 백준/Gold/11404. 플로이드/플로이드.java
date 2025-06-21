64f8178d5b38d3f3ac4601a670f3a5845461a310
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-06-21 15:05:10 +0900

    [Gold IV] Title: 플로이드, Time: 480 ms, Memory: 53644 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,72 +1,63 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-class Main {
+public class Main {
 
-    int[][] matrix;
-    int n;
-    int m;
+    // Default Setting Object
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
 
-    public void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
 
+    public static void main(String[] args) throws IOException {
+        // Init
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
-        matrix = new int[n + 1][n + 1];
 
-        for (int[] m : matrix)
-            Arrays.fill(m, 100000 * 1000);
-        for (int i = 1; i <= n; i++)
-            matrix[i][i] = 0;
+        int[][] matrix = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++){
+                if (i == j) {
+                    matrix[i][i] = 0;
+                } else {
+                    matrix[i][j] = 100000001;
+                }
+            }
+        }
 
         for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            String[] tmp = br.readLine().split(" ");
+            int x = Integer.parseInt(tmp[0]);
+            int y = Integer.parseInt(tmp[1]);
+            int c = Integer.parseInt(tmp[2]);
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            if (matrix[a][b] == 100000 * 1000)
-                matrix[a][b] = c;
-            else
-                matrix[a][b] = Math.min(matrix[a][b], c);
-        }
-
-        floyd();
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                int x = matrix[i][j];
-                if (x == 100000 * 1000) x = 0;
-                sb.append(x).append(" ");
+            if (matrix[x][y] > c) {
+                matrix[x][y] = c;
             }
-            sb.append("\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    public void floyd() {
+        // Floyd
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
-                    if (i == j) continue;
                     matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
                 }
             }
         }
-    }
 
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
+        // Output
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i][j] == 100000001) {
+                    bw.write("0 ");
+                } else {
+                    bw.write(matrix[i][j] + " ");
+                }
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
