b8b0d317687d32da2e179f6904537a94e52bc58e
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-06-28 00:21:59 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,20 @@
+WITH RECURSIVE CTE AS (
+    SELECT 
+        ID,
+        PARENT_ID,
+        1 AS LEVEL
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
+    
+    UNION ALL
+    
+    SELECT 
+        A.ID,
+        A.PARENT_ID,
+        CTE.LEVEL + 1
+    FROM ECOLI_DATA A INNER JOIN CTE
+    ON A.PARENT_ID = CTE.ID
+)
+SELECT ID
+FROM CTE
+WHERE LEVEL = 3
