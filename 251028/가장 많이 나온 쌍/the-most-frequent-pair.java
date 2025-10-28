f1f6a74c98e6422ffb929b6d101d14228fd2618d
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-10-28 17:17:25 +0900

    Title: 가장 많이 나온 쌍, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 251028/가장 많이 나온 쌍/the-most-frequent-pair.java
+++ 251028/가장 많이 나온 쌍/the-most-frequent-pair.java
@@ -1,14 +1,19 @@
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
 
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < M; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
             String tmp;
             if (a > b) tmp = b + " " + a;
             else tmp = a + " " + b;
