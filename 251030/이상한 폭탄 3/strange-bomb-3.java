f96a81bff39d87e2c02d3f8f7c22eedd21fe1b96
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-10-30 12:42:06 +0900

    Title: 이상한 폭탄 3, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251030/이상한 폭탄 3/strange-bomb-3.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] bombs = new int[n];
+        for (int i = 0; i < n; i++)
+            bombs[i] = sc.nextInt();
+
+        boolean[] visited = new boolean[n];
+        int maxCnt = 0;
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            int b = bombs[i];
+            int cnt = 1;
+
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+
+            while (!q.isEmpty()) {
+                int idx = q.poll();
+                for (int j = idx + 1; j <= idx + k && j < n; j++) {
+                    if (visited[idx]) continue;
+                    if (b == bombs[j]) {
+                        cnt++;
+                        q.add(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+
+            if (cnt == 1) {
+                answer = 0;
+            } else if (maxCnt == cnt) {
+                if (answer < b) {
+                    answer = b;
+                }
+            } else if (maxCnt < cnt) {
+                answer = b;
+                maxCnt = cnt;
+            }
+        }
+        System.out.println(answer);
+    }
+}
