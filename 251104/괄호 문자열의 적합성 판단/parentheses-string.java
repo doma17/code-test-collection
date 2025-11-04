6d5d30b8cf0632b8f8ccfee651db5dd2ba39306a
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-11-04 20:36:44 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 117ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251104/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            
+            if (c == '(') {
+                st.push(1);
+            } else if (c == ')') {
+                st.pop();
+            }
+        }
+
+        if (st.isEmpty()) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
