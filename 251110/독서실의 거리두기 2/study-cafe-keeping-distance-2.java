f04326d8f41049b01fc8efe3743f3bec08d4267a
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-11-10 14:56:16 +0900

    Title: 독서실의 거리두기 2, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251110/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
+++ 251110/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -4,8 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String seats = sc.next();
+        String s = sc.next();
         
-        char[] arr = seats.toCharArray();
+        char[] arr = s.toCharArray();
+        int maxLength = 0;
+        int minLength = 10001;
+        int first = -1;
+        int last = -1;
+        
+        int curLength = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '1') {
+                if (first == -1) {
+                    first = i;
+                } else {
+                    maxLength = Math.max(maxLength, curLength);
+                    minLength = Math.min(minLength, curLength);
+                }
+                curLength = 0;
+                last = i;
+            }
+            curLength++;
+        }
+        
+        int maxDist = first;
+
+        int lastLength = (arr.length - 1) - last;
+        maxDist = Math.max(maxDist, lastLength);
+        int midLength = (maxLength + 1) / 2;
+        maxDist = Math.max(maxDist, midLength);
+
+        if (maxDist < minLength) System.out.println(maxDist);
+        else System.out.println(minLength);
     }
 }
