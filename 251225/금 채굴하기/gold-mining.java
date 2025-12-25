70d826ed8270276e8ad223501c6da0358c57f928
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-12-25 14:11:45 +0900

    Title: 금 채굴하기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251225/금 채굴하기/gold-mining.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        int answer = -1;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                for (int k = 1; k < n; k++) {
+                    answer = Math.max(answer, getMaxGoldInK(r, c, k));
+                }
+            }
+        }
+        System.out.println(answer);
+     }
+
+    static int[][] grid;
+    static int n;
+    static int m;
+
+    private static int getMaxGoldInK(int r, int c, int k) {
+        int gold = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (Math.abs(r - i) + Math.abs(c - j) <= k) {
+                    gold += grid[i][j];
+                }
+            }
+        }
+        int goldCost = gold * m;
+        int digCost = k * k + (k + 1) * (k + 1);
+
+        if (digCost >= goldCost) return -1;
+        return gold;
+    }
+}
