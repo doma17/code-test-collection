72d56ecea670857192fcb24f8d027333d35cbe42
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-12-30 12:25:25 +0900

    Title: 삼각형 컨베이어 벨트, Time: 197ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 251230/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int[] arr = new int[n * 3];
+        for (int i = 0; i < n * 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        while (t-- > 0) {
+            int temp = arr[n * 3 - 1];
+            for (int i = n * 3 - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = temp;
+        }
+
+        for (int i = 0; i < n * 3; i++) {
+            System.out.print(arr[i] + " ");
+            if ((i + 1) % n == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
