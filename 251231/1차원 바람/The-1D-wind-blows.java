7a3271cb7a3e50baae5aa77992f55aba01a1235c
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2025-12-31 20:51:36 +0900

    Title: 1차원 바람, Time: 353ms, Memory: 22MB, Status: Passed - Codetree

--- 251231/1차원 바람/The-1D-wind-blows.java
+++ 251231/1차원 바람/The-1D-wind-blows.java
@@ -19,7 +19,7 @@ public class Main {
         for (int i = 0; i < q; i++) {
             int r = sc.nextInt();
             char d = sc.next().charAt(0);
-            cacluateWind(r - 1, d);
+            calculateWind(r - 1, d == 'L' ? 1 : -1);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -27,73 +27,55 @@ public class Main {
             for (int j = 0; j < m; j++) {
                 sb.append(a[i][j] + " ");
             }
-            sb.append("\n");
+            if (i != n - 1) sb.append("\n");
         }
         System.out.println(sb.toString());
     }
 
-    private static void cacluateWind(int r, char d) {
-        int[] dirs = new int[n];
-        dirs[r] = (d == 'L' ? 1 : -1);
+    private static void calculateWind(int r, int dir) {
+        shift(r, dir);
 
+        int currDir = dir;
         for (int i = r; i > 0; i--) {
-            if (dirs[i] == 0) break;
-
-            boolean isSame = false;
-            for (int c = 0; c < m; c++) {
-                if (a[i][c] == a[i - 1][c]) {
-                    isSame = true;
-                    break;
-                }
-            }
-
-            if (isSame) {
-                dirs[i - 1] = -dirs[i];
+            if (hasSameNumber(i, i - 1)) {
+                currDir = -currDir;
+                shift(i - 1, currDir);
             } else {
                 break;
             }
         }
 
+        currDir = dir;
         for (int i = r; i < n - 1; i++) {
-            if (dirs[i] == 0) break;
-
-            boolean isSame = false;
-            for (int c = 0; c < m; c++) {
-                if (a[i][c] == a[i + 1][c]) {
-                    isSame = true;
-                    break;
-                }
-            }
-
-            if (isSame) {
-                dirs[i + 1] = -dirs[i];
+            if (hasSameNumber(i, i + 1)) {
+                currDir = -currDir;
+                shift(i + 1, currDir);
             } else {
                 break;
             }
         }
+    }
 
-        for (int i = 0; i < n; i++) {
-            if (dirs[i] == 1) {
-                windLeft(i);
-            } else if (dirs[i] == -1) {
-                windRight(i);
+    private static boolean hasSameNumber(int r1, int r2) {
+        for (int i = 0; i < m; i++) {
+            if (a[r1][i] == a[r2][i]) return true;
+        }
+        return false;
+    }
+
+    private static void shift(int r, int dir) {
+        if (dir == 1) {
+            int temp = a[r][m - 1];
+            for (int i = m - 1; i > 0; i--) {
+                a[r][i] = a[r][i - 1];
             }
+            a[r][0] = temp;
+        } else {
+            int temp = a[r][0];
+            for (int i = 0; i < m - 1; i++) {
+                a[r][i] = a[r][i + 1];
+            }
+            a[r][m - 1] = temp;
         }
-    }
-
-    private static void windLeft(int r) {
-        int temp = a[r][m - 1];
-        for (int i = m - 1; i > 0; i--) {
-            a[r][i] = a[r][i - 1];
-        }
-        a[r][0] = temp;
-    }
-
-    private static void windRight(int r) {
-        int temp = a[r][0];
-        for (int i = 0; i < m - 1; i++) {
-            a[r][i] = a[r][i + 1];
-        }
-        a[r][m - 1] = temp;
     }
 }
