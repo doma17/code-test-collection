e782e325d22fe1e83962537c6970368d7e8bf784
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-01-09 21:33:53 +0900

    Title: 2차원 폭발 게임, Time: 565ms, Memory: 28MB, Status: Wrong Answer - Codetree

---
+++ 260109/2차원 폭발 게임/The-2D-bomb-game.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class Main {
+
+    static int n, m, k;
+    static int[][] grid;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        for (int i = 0; i < k; i++) {
+            bomb();
+            gravity();
+            pivot();
+            gravity();
+        }
+        bomb();
+
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(grid[i][j] + " ");
+                if (grid[i][j] != 0) answer++;
+            }
+                
+            if (i + 1 != n) sb.append("\n");
+        }
+        // System.out.println(sb);
+        System.out.println(answer);
+    }
+
+    // 폭탄 터지기 로직
+    private static void bomb() {
+        // 열 기준으로 연속 m개인 곳을 찾아서 0으로 변환
+        for (int j = 0; j < n; j++) {
+            int i = 0;
+            while (i < n) {
+                if (grid[i][j] == 0) {
+                    i++;
+                    continue;
+                }
+                int end = i + 1;
+                while (end < n && grid[end][j] == grid[i][j]) {
+                    end++;
+                }
+
+                if (end - i >= m) {
+                    for (int x = i; x < end; x++){
+                        grid[x][j] = 0;
+                    }
+                }
+                i = end;
+            }
+        }
+    }
+
+    // 중력 로직
+    private static void gravity() {
+        int[][] tmp = new int[n][n];
+        for (int j = 0; j < n; j++) {
+            int idx = n - 1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (grid[i][j] != 0) tmp[idx--][j] = grid[i][j];   
+            }
+        }
+        grid = tmp;
+    }
+
+    // 피봇
+    private static void pivot() {
+        // 시계방향으로 90도 회전
+        int[][] tmp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                tmp[j][n - 1 - i] = grid[i][j];
+        } 
+        grid = tmp;
+    }
+}
