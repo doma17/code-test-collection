c353cea9a403d48fe83f4b0de0c219a83965dcba
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-01-12 20:45:01 +0900

    Title: 떨어지는 1자 블록, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260112/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        
+
+        int contactR = 0;
+        for (int r = 0; r < n; r++) {
+            boolean isContacted = false;
+            for (int c = k - 1; c < k + m - 1; c++) {
+                if (grid[r][c] == 1) {
+                    isContacted = true;
+                    break;
+                }
+            }
+
+            if (isContacted) {
+                contactR = r - 1;
+                break;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (contactR >= 0) {
+            for (int c = k - 1; c < k + m - 1; c++) {
+                grid[contactR][c] = 1;
+            }
+        }
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) 
+                sb.append(grid[r][c]).append(" ");
+            if (r + 1 < n) sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
