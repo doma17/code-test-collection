7dc28ef362001926cb7aa8b3ddb57fe095122fd5
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-01-27 20:39:48 +0900

    Title: 겹치지 않게 선분 고르기, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260127/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+
+    static class Line implements Comparable<Line> {
+        int l, r;
+
+        public Line(int l, int r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        public int compareTo(Line o) {
+            if (this.l != o.l) return this.l - o.l;
+            return this.r = o.r;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] segments = new int[n][2];
+
+        ArrayList<Line> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new Line(sc.nextInt(), sc.nextInt()));
+        }
+        Collections.sort(list);
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            int last = list.get(i).r;
+
+            for (int j = i + 1; j < n; j++) {
+                Line o = list.get(j);
+                if (last < o.l) {
+                    last = o.r;
+                    count++;
+                }
+            }
+            answer = Math.max(answer, count);
+        }
+        System.out.println(answer);
+    }
+}
