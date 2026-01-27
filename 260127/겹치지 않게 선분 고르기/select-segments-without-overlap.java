5b71e90628d7c98820378e8bb78cf3bf95d558a0
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-01-27 20:52:05 +0900

    Title: 겹치지 않게 선분 고르기, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 260127/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 260127/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
@@ -16,14 +17,16 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[][] segments = new int[n][2];
 
         ArrayList<Line> list = new ArrayList<>();
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            list.add(new Line(sc.nextInt(), sc.nextInt()));
+            st = new StringTokenizer(br.readLine());
+            list.add(new Line(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
         Collections.sort(list);
 
