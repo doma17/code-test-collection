c1834c8e5795e64b52867c15fc2a556877a70dc7
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-02-12 19:52:00 +0900

    Title: 외판원 순회, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260212/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[][] cost;
+    static boolean[] visited;
+
+    static int answer = 100001;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        cost = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cost[i][j] = sc.nextInt();
+            }
+        }
+        visited = new boolean[n];
+        choose(0, 0, 0);
+        System.out.println(answer);
+    }
+
+    private static void choose(int idx, int sum, int spot) {
+        if (idx ==  n && spot == 0) {
+            answer = Math.min(answer, sum);
+            return;
+        }
+
+        int start = (idx == n - 1) ? 0 : 1;
+        for (int i = start; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            choose(idx + 1, sum + cost[spot][i], i);
+            visited[i] = false;
+        }
+    }
+}
