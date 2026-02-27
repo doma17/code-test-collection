0e596366ecac68dc7f7cf63eb9288188524d06f8
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-02-27 15:15:57 +0900

    Title: 최소 경로로 탈출 하기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260227/최소 경로로 탈출 하기/escape-with-min-distance.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Main {
+
+    static int n, m;
+    static int[][] a;
+    static int answer = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        a = new int[n][m];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                a[i][j] = sc.nextInt();
+        
+        System.out.println(bfs(0, 0));
+    }
+
+    static int[] dr = {0, 0, -1, 1};
+    static int[] dc = {1, -1, 0, 0};
+
+    private static int bfs(int r, int c) {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+        int[][] step = new int[n][n];
+        q.add(new int[] {r, c});
+        visited[r][c] = true;
+        
+
+        while (!q.isEmpty()) {
+            int[] next = q.poll();
+            
+            for (int i = 0; i < 4; i++) {
+                int nr = next[0] + dr[i];
+                int nc = next[1] + dc[i];
+
+                if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
+                if (visited[nr][nc]) continue;
+                if (a[nr][nc] == 0) continue; 
+
+                visited[nr][nc] = true;
+                step[nr][nc] = step[next[0]][next[1]] + 1; 
+                q.add(new int[] {nr, nc});
+            }
+        }
+        return step[n - 1][n - 1];
+    }
+}
