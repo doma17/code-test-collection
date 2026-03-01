d39e7ad222ce2b610d6e875a615c306c28bb7ad4
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-03-01 14:04:51 +0900

    Title: 비를 피하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260301/비를 피하기/stay-out-of-rain.java
+++ 260301/비를 피하기/stay-out-of-rain.java
@@ -3,16 +3,48 @@ import java.util.*;
 public class Main {
 
     static int n, h, m;
+    static int[][] a;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         h = sc.nextInt();
         m = sc.nextInt();
-        int[][] a = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
+        a = new int[n][n];
+
+        ArrayList<int[]> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 a[i][j] = sc.nextInt();
-        // Please write your code here.
+                if (a[i][j] == 2)
+                    list.add(new int[] {i, j});
+            }
+        }
+        
+        int[][] result = new int[n][n];
+        for (int[] human : list) {
+            result[humna[0]][human[1]] = bfs(human[0], human[1]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) sb.append(result[i][j] + " ");
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int bfs(int r, int c) {
+        int step = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {r, c});
+        boolean[][] visited = new boolean[n][n];
+        visited[r][c] = true;
+
+        while (!q.isEmpty()) {
+            int[] next = q.poll();
+            
+            for ()
+        }
     }
 }
