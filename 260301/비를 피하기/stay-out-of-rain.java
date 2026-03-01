38cd3786d57cccdcd9e362926a9624c8fee33159
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-03-01 13:47:55 +0900

    Title: 비를 피하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260301/비를 피하기/stay-out-of-rain.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+    static int n, h, m;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        h = sc.nextInt();
+        m = sc.nextInt();
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                a[i][j] = sc.nextInt();
+        // Please write your code here.
+    }
+}
