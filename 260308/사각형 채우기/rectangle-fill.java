254e43d478e7830bde07410296233bb31144d7ca
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-03-08 21:19:37 +0900

    Title: 사각형 채우기, Time: 140ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260308/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+
+    static int[] memo;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        System.out.println(dp(n));
+    }
+
+    private static int dp(int num) {
+        if (num == 1) memo[1] = 1;
+        if (num == 2) memo[2] = 2;
+        if (num == 3) memo[3] = 3;
+        if (memo[num] == -1)
+            memo[num] = dp(num - 1) + dp(num - 2);
+        return memo[num];
+    }
+}
