c77eec6d69eecfa0f963996e2f096266e1a8323f
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-04-07 22:10:36 +0900

    Title: 최대 동전 거슬러주기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/최대 동전 거슬러주기/max-coin-change.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        
+        int[] dp = new int[m + 1];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= m) dp[arr[i]] = 1;
+        }
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i < arr[j]) break;
+                if (dp[i - arr[j]] == 0) continue;
+                dp[i] = Math.max(dp[i], dp[i - arr[j]] + 1);
+            }
+        }
+        // for (int i = 0; i <= m; i++) System.out.print(i + " ");
+        // System.out.println();
+        // for (int i = 0; i <= m; i++) System.out.print(dp[i] + " ");
+        // System.out.println();
+
+        int result = dp[m];
+        if (dp[m] == 0) result = -1;
+        System.out.println(result);
+    }
+}
