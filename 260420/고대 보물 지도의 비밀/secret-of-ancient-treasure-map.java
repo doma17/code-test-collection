9c449bc4beba6d584da4aaa6323eb80338365bdb
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-04-20 14:56:11 +0900

    Title: 고대 보물 지도의 비밀, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260420/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ 260420/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -15,20 +15,39 @@ public class Main {
         int[][] dp = new int[N][K + 1];
         for (int i = 0; i < N; i++)
             Arrays.fill(dp[i], MIN);
-        dp[0][0] = 1;
+        if (arr[0] >= 0)
+            dp[0][0] = arr[0];
+        else 
+            dp[0][1] = arr[0];
 
-        for (int i = 0; i < N; i++) {
-            int target = arr[i];
-
-            if (target >= 0) {
+        for (int i = 1; i < N; i++) {
+            if (arr[i] >= 0) {
                 for (int j = 0; j <= K; j++) {
-                    dp[i][j] = 
+                    if (dp[i - 1][j] != MIN) {
+                        dp[i][j] = dp[i - 1][j] + arr[i];
+                    }
+                    if (j == 0) {
+                        dp[i][j] = Math.max(dp[i][j], arr[i]);
+                    }
                 }
             } else {
-                for (int j = 0; j <= K; j++) {
-                    
+                for (int j = 0; j < K; j++) {
+                    if (dp[i - 1][j] != MIN) {
+                        dp[i][j + 1] = dp[i - 1][j] + arr[i];
+                    }
+                }
+                if (K >= 1) {
+                    dp[i][1] = Math.max(dp[i][1], arr[i]);
                 }
             }
         }
+
+        int answer = MIN;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= K; j++) {
+                answer = Math.max(answer, dp[i][j]);
+            }
+        }
+        System.out.println(answer);
     }
 }
