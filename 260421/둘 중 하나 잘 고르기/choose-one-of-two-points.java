c8d3c922932aa0d4b97a3b5f6ac756ed061813ba
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-04-21 22:22:55 +0900

    Title: 둘 중 하나 잘 고르기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260421/둘 중 하나 잘 고르기/choose-one-of-two-points.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] dp = new int[2 * n + 1][2];
+        Arrays.fill(dp)
+
+        for (int i = 0; i < 2 * n; i++) {
+            int red = sc.nextInt();
+            int blue = sc.nextInt();
+
+        }
+    }
+}
