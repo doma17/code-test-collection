3d2c35765e5c3755eedd65abd0d5571f68d00351
Kwak Byoung Min <67214970+doma17@users.noreply.github.com>
2026-04-21 22:32:00 +0900

    Title: 둘 중 하나 잘 고르기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/둘 중 하나 잘 고르기/choose-one-of-two-points.java
+++ 260421/둘 중 하나 잘 고르기/choose-one-of-two-points.java
@@ -1,16 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[][] dp = new int[2 * n + 1][2];
-        Arrays.fill(dp)
+        int[][] dp = new int[2 * n + 1][n + 1];
+        for (int i = 0; i <= 2 * n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        dp[0][0] = 0;
 
-        for (int i = 0; i < 2 * n; i++) {
+        for (int i = 1; i <= 2 * n; i++) {
             int red = sc.nextInt();
             int blue = sc.nextInt();
 
+            for (int j = 0; j <= n; j++) {
+                int blueCount = i - j - 1;
+
+                if (j > 0 && dp[i - 1][j - 1] != -1) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + red);
+                }
+
+                if (blueCount < n && dp[i - 1][j] != -1) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j] + blue);
+                }
+            }
         }
+
+        System.out.println(dp[2 * n][n]);
     }
 }
